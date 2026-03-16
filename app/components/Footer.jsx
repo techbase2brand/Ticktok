9b221cc5f21bf4e0355e1338d0ce2e51b950f762
techbase2brand <techbase2brand@gmail.com>
2026-03-16 21:04:06 +0530

    added pages

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -2,7 +2,6 @@
 import { memo, useState } from 'react';
 import Image from "next/image";
 import Link from "next/link";
-
 const Footer = ({copyRight}) => {
     const [isQuickLinksOpen, setIsQuickLinksOpen] = useState(false);
     const [isLegalOpen, setIsLegalOpen] = useState(false);
