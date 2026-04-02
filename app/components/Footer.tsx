036e5c32d57d50f22ca37e1b8f93f4ad6cc0dfec
techbase2brand <techbase2brand@gmail.com>
2026-04-02 18:20:38 +0530

    fix type script error

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -333,7 +333,6 @@ const Footer = ({ copyRight }: FooterProps) => {
                                 sim@ticktockloans.com.au
                             </a>
                         </div>
-
                         {/* Phone 1 */}
                         <div className='flex items-center gap-3'>
                             <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className="flex-shrink-0">
