499ed23b0286c257517d53f6511ddc161463156a
saurabhbase2brand-lang <saurabhbase2brand@gmail.com>
2026-04-07 15:05:46 +0530

    update-file

--- app/components/Contactform/Contactform.tsx
+++ app/components/Contactform/Contactform.tsx
@@ -15,7 +15,6 @@ const ContactPage = ({ title, subheading, description }: ContactPageProps) => {
       </g>
     </svg>
   ));
-
   const [formData, setFormData] = useState({
     firstName: '',
     lastName: '',
